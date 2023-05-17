96de4f950a6e9d825d3c55fd9061ece537760a7b
VictorMello1993 <victorsmello93@gmail.com>
2023-05-17 11:26:00 -0300

    feat: main page finished

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,42 +1,63 @@
 import { User } from "lucide-react";
-import Image from "next/image";
 import logo from "../assets/nlw-spacetime-logo.svg";
+import Image from "next/image";
 
-export default function Home () {
+export default function Home() {
   return (
-    <main className="grid grid-cols-2 min-h-screen">
+    <main className="grid min-h-screen grid-cols-2">
       {/* esquerda */}
-      <div className="bg-[url(../assets/bg-stars.svg)] bg-coverflex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10">
+      <div className="bg-coverflex relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] px-28 py-16">
         {/* Blur */}
-        <div className="absolute right-0 top-1/2 -translate-y-1/2 translate-x-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 rounded-full blur-full"/>
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
 
         {/* Stripes */}
-        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes"/>
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
         {/* Sign in */}
-        <a href="" className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors">
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
           <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500"/>
+            <User className="h-5 w-5 text-gray-500" />
           </div>
 
-          <p className="text-sm leading-snug max-w-[140px]"><span className="underline">Crie sua conta</span> e salve suas memórias!</p>
+          <p className="max-w-[140px] text-sm leading-snug">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias!
+          </p>
         </a>
 
         {/* Slogan */}
         <div className="space-y-5">
           <Image src={logo} alt="NLW Spacetime"/>
-          <div>
-            <h1>Sua cápsula do tempo</h1>
-            <p>Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</p>
+          <div className="max-w-[420px] space-y-1">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">Sua cápsula do tempo</h1>
+            <p className="text-lg leading-relaxed">Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</p>
           </div>
-          <a href="">CADASTRAR LEMBRANÇA</a>
+          <a className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600" href="">CADASTRAR LEMBRANÇA</a>
+        </div>
+
+        {/* Copyright */}
+        <div className="text-sm leading-relaxed text-gray-200">Feito com 💜 no NLW da {" "}
+        <a href="https://rocketseat.com.br"
+           className="underline hover:text-gray-100"
+           target="_blank"
+           rel="noreferrer">
+          Rocketseat</a>
         </div>
       </div>
 
       {/* direita */}
-      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
+      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
         <div className="flex flex-1 items-center justify-center">
-          <p className="text-center leading-relaxed w-[360px]">Você ainda não registrou nenhuma lembrança, comece a <a className="underline hover:text-gray-50" href="">criar agora</a>!</p>
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{" "}
+            <a className="underline hover:text-gray-50" href="">
+              criar agora
+            </a>
+            !
+          </p>
         </div>
       </div>
     </main>
